7b2167099269b7e8ed023b384d80a5356b71ae56
Martin Dong <DongSky@users.noreply.github.com>
2022-05-10 11:15:09 +0800

    Update structure.py

--- PromptModels/structure.py
+++ PromptModels/structure.py
@@ -6,11 +6,27 @@ import torch.nn as nn
 from timm.models.vision_transformer import VisionTransformer, PatchEmbed
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
+# class Prompt(nn.Module):
+#     def __init__(self, type='Deep', depth=12, channel=768, length=10):
+#         super().__init__()
+#         self.prompt = nn.Parameter(torch.zeros(depth, length, channel))
+#         trunc_normal_
+
+class PromptLearner(nn.Module):
+    def __init__(self, num_classes, prompt_length, prompt_depth, prompt_channels):
+        super().__init__()
+        self.Prompt_Tokens = nn.Parameter(torch.zeros(prompt_depth, prompt_length, prompt_channels))
+        self.head = nn.Linear(prompt_channels, num_classes)
+        trunc_normal_(self.Prompt_Tokens, std=.02)
+        trunc_normal_(self.head.weight, std=.02)
+    def forward(self, x):
+        return self.head(x)
+
 class VPT_ViT(VisionTransformer):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
-                 num_heads=8, mlp_ratio=4., qkv_bias=True, representation_size=None, distilled=False,
+                 num_heads=12, mlp_ratio=4., qkv_bias=True, representation_size=None, distilled=False,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., embed_layer=PatchEmbed, norm_layer=None,
-                 act_layer=None, weight_init='', Prompt_Token_num=1, VPT_type="Shallow"):
+                 act_layer=None, weight_init='', Prompt_Token_num=1, VPT_type="Deep"):
 
         # Recreate ViT
         super().__init__(img_size, patch_size, in_chans, num_classes, embed_dim, depth, num_heads, mlp_ratio, qkv_bias,
@@ -18,23 +34,26 @@ class VPT_ViT(VisionTransformer):
                          norm_layer, act_layer, weight_init)
 
         self.VPT_type = VPT_type
-        if VPT_type == "Deep":
-            self.Prompt_Tokens = nn.Parameter(torch.zeros(depth, Prompt_Token_num, embed_dim))
-        else:  # "Shallow"
-            self.Prompt_Tokens = nn.Parameter(torch.zeros(1, Prompt_Token_num, embed_dim))
-        trunc_normal_(self.Prompt_Tokens, std=.02)
+        # if VPT_type == "Deep":
+        #     self.Prompt_Tokens = nn.Parameter(torch.zeros(depth, Prompt_Token_num, embed_dim))
+        # else:  # "Shallow"
+        #     self.Prompt_Tokens = nn.Parameter(torch.zeros(1, Prompt_Token_num, embed_dim))
+        # trunc_normal_(self.Prompt_Tokens, std=.02)
+        self.dropout = nn.Dropout(0.1)
+        self.prompt_learner = PromptLearner(num_classes, Prompt_Token_num, depth, embed_dim)
+        self.head = nn.Identity()
 
-    def New_CLS_head(self, new_classes=15):
-        self.head = nn.Linear(self.embed_dim, new_classes)
-        trunc_normal_(self.head.weight, std=.02)
+    # def New_CLS_head(self, new_classes=15):
+    #     self.head = nn.Linear(self.embed_dim, new_classes)
+    #     trunc_normal_(self.head.weight, std=.02)
 
     def Freeze(self):
         for param in self.parameters():
-            param.requires_grad = False
+            param.requires_grad_(False)
 
-        self.Prompt_Tokens.requires_grad = True
-        for param in self.head.parameters():
-            param.requires_grad = True
+        # self.Prompt_Tokens.requires_grad_(True)
+        for param in self.prompt_learner.parameters():
+            param.requires_grad_(True)
 
     def obtain_prompt(self):
         prompt_state_dict = {'head': self.head.state_dict(),
@@ -57,11 +76,11 @@ class VPT_ViT(VisionTransformer):
 
         if self.VPT_type == "Deep":
 
-            Prompt_Token_num = self.Prompt_Tokens.shape[1]
+            Prompt_Token_num = self.prompt_learner.Prompt_Tokens.shape[1]
 
             for i in range(len(self.blocks)):
                 # concatenate Prompt_Tokens
-                Prompt_Tokens = self.Prompt_Tokens[i].unsqueeze(0)
+                Prompt_Tokens = self.prompt_learner.Prompt_Tokens[i].unsqueeze(0)
                 x = torch.cat((x, Prompt_Tokens.expand(x.shape[0], -1, -1)), dim=1)
                 num_tokens = x.shape[1]
                 x = self.blocks[i](x)[:, :num_tokens - Prompt_Token_num]
@@ -78,5 +97,7 @@ class VPT_ViT(VisionTransformer):
 
     def forward(self, x):
         x = self.forward_features(x)
+        x = self.dropout(x)
+        x = self.prompt_learner(x)
         x = self.head(x)
         return x
